e5e9b3725edbf306b16ae2b8e36535867d761f20
Steve-the-Jobless <aravindsrinivasan183@gmail.com>
2019-03-23 20:27:46 +0530

    ui

--- app/src/main/java/com/example/smartsurvey/DashboardActivity.java
+++ app/src/main/java/com/example/smartsurvey/DashboardActivity.java
@@ -1,7 +1,11 @@
 package com.example.smartsurvey;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        TextView newSurveyView = (TextView) findViewById(R.id.newSurveyView);
+        newSurveyView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SurveyPlanner.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
